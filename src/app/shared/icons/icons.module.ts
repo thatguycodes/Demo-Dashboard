4eedc86b6884e151adbbd158a1d0f5b0e4ea179b
ltobonye <ltobonye@gmail.com>
2020-03-07 22:42:03 +0200

    add table to display the companies or clients

--- src/app/shared/icons/icons.module.ts
+++ src/app/shared/icons/icons.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FeatherModule } from 'angular-feather';
-import { Camera, Heart, Github, Home, File, ShoppingCart, Users, BarChart2, Layers, PlusCircle, FileText, Activity, Briefcase } from 'angular-feather/icons';
+import { Camera, Heart, Github, Home, File, ShoppingCart, Users, BarChart2, Layers, PlusCircle, FileText, Activity, Briefcase, ChevronLeft } from 'angular-feather/icons';
 
 // Select some icons (use an object, not an array)
 const icons = {
@@ -17,7 +17,8 @@ const icons = {
   PlusCircle,
   FileText,
   Briefcase,
-  Activity 
+  Activity,
+  ChevronLeft 
 };
 
 @NgModule({
